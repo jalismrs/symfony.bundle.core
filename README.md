796c8332c13b9336051d6c8bc0ff401dbed38e5e
kevin.castejon <kevin.castejon@jalis.fr>
2020-11-03 14:24:41 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # symfony.bundle.core
+
 Adds core recipe
+
+## Configuration
+```yaml
+# config/services.yaml
+
+parameters:
+    'app.name': 'APP_NAME'
+    'app.version': 'APP_VERSION'
+```
---
+++ src/JalismrsCoreBundle.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+namespace Jalismrs\Symfony\Bundle\JalismrsCoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class JalismrsCoreBundle
+ *
+ * @package Jalismrs\Symfony\Bundle\JalismrsCoreBundle
+ */
+class JalismrsCoreBundle extends
+    Bundle
+{
+
+}
